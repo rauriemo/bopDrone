5161a12bc19c082ab5a2f544e19ce065c4043a55
rauriemo <rafael.auriemo@gmail.com>
2015-09-13 22:27:16 -0300

    initial commit skeleton and app.js

---
+++ config.js
@@ -0,0 +1,17 @@
+var config = {
+        plugins: [
+          "pilot",
+          // "battery",
+          // "video-feed",
+          // "flight-recorder",
+          // "HUD"
+
+        ],
+
+        // flightrecorder: {
+        //   path: "./videos"
+        // }
+
+};
+
+module.exports = config;
